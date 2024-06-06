2474d05d3cff29b3f4c6070db24afae51fca8f93
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-06 23:25:36 +0900

    サンプルコードコピペ

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,24 @@ import { OpenAIClient, AzureKeyCredential } from "@azure/openai"
 function App() {
   const [count, setCount] = useState(0)
 
+  async function main(){
+    const client = new OpenAIClient(
+    "https://opendialogue1.openai.azure.com/",
+    new AzureKeyCredential("e1a905c26e7d418bb8ce8f95518c9f45"));
+  
+    const { choices } = await client.getCompletions(
+      "gpt35turbo", // assumes a matching model deployment or model name
+      ["Hello, world!"]);
+  
+    for (const choice of choices) {
+      console.log(choice.text);
+    }
+  }
+  
+  main().catch((err) => {
+    console.error("The sample encountered an error:", err);
+  });
+
   return (
     <>
       {/* <div>
