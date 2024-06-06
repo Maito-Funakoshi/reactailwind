fefc0bd3419719f7a089db83358fe8dd792cb37f
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-06 23:07:34 +0900

    require → import

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { OpenAIClient, AzureKeyCredential } from "@azure/openai"
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
   return (
     <>
       {/* <div>
