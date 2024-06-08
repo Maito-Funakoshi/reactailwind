06e5a426761593d128359455ef46eca8a777eb91
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-08 19:20:19 +0900

    再挑戦

--- src/Header.jsx
+++ src/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({ messages }) => {
   return (
     <h1>
       <span id="title">Self Disclosure Chatbot</span>
-      <button onClick={handleGetLog}><img src="images/downloadIcon.jpg" alt="ダウンロード" /></button>
+      <button onClick={handleGetLog}><img src="../images/downloadIcon.jpg" alt="ダウンロード" /></button>
     </h1>
   );
 };
