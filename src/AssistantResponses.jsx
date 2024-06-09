6c107f0ba356766e4cd7744f56dba0a79c3d22af
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-09 21:54:07 +0900

    ５

--- src/AssistantResponses.jsx
+++ src/AssistantResponses.jsx
@@ -6,11 +6,8 @@ const AssistantResponses = ({ messages, setMessages, names, characters, setError
   const azureApiKey = `e1a905c26e7d418bb8ce8f95518c9f45`;
   const deploymentId = "gpt35turbo";
 
-//   const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
 
-  const client0 = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-  const client1 = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-  const client2 = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
 //   let name;
 //   let character;
 
@@ -36,7 +33,7 @@ const AssistantResponses = ({ messages, setMessages, names, characters, setError
                 const modifiedMessages = messages.slice(1);
                 messages0 = [...messages0, ...modifiedMessages];
                 //返信を作成
-                const response0 = await client0.getChatCompletions(deploymentId, messages0, { maxTokens: 256 });
+                const response0 = await client.getChatCompletions(deploymentId, messages0, { maxTokens: 256 });
                 //ログに追加
                 if (response0.choices && response0.choices.length > 0) {
                     const botMessage0 = response0.choices[0].message.content.trim();
@@ -49,7 +46,7 @@ const AssistantResponses = ({ messages, setMessages, names, characters, setError
                 const modifiedMessages = messages.slice(1);
                 messages1 = [...messages1, ...modifiedMessages];
                 //返信を作成
-                const response1 = await client1.getChatCompletions(deploymentId, messages1, { maxTokens: 256 });
+                const response1 = await client.getChatCompletions(deploymentId, messages1, { maxTokens: 256 });
                 //ログに追加
                 if (response1.choices && response1.choices.length > 0) {
                     const botMessage1 = response1.choices[0].message.content.trim();
@@ -62,7 +59,7 @@ const AssistantResponses = ({ messages, setMessages, names, characters, setError
                 const modifiedMessages = messages.slice(1);
                 messages2 = [...messages2, ...modifiedMessages];
                 //返信を作成
-                const response2 = await client2.getChatCompletions(deploymentId, messages1, { maxTokens: 256 });
+                const response2 = await client.getChatCompletions(deploymentId, messages1, { maxTokens: 256 });
                 //ログに追加
                 if (response2.choices && response2.choices.length > 0) {
                     const botMessage2 = response2.choices[0].message.content.trim();
