75ab77b17b21a800131dbc0931c55cf1ea637e67
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-11 01:49:56 +0900

    リフレクティングモード切り替えボタン設置

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import downloadIcon from "../images/downloadIcon.png"
 
+const switchOuter = document.querySelector(".switch_outer");
+const toggleSwitch = document.querySelector(".toggle_switch");
+
+switchOuter.addEventListener("click", () => {
+  switchOuter.classList.toggle("active");
+  toggleSwitch.classList.toggle("active");
+});
+
 const Header = ({ messages }) => {
   const handleGetLog = () => {
     const log = messages.slice(1).map((msg) => msg.content).join('\n');
@@ -20,6 +28,9 @@ const Header = ({ messages }) => {
     <header className="App-header">
       <h1>
         <span id="title">Self Disclosure Chatbot</span>
+        <div class="switch_outer">
+          <div class="toggle_switch"></div>
+        </div>
         <button onClick={handleGetLog}><img src={downloadIcon} alt="ダウンロード" /></button>
       </h1>
     </header>
