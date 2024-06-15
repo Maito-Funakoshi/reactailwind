da159c471c051bf9f63cbc0b97656f4f4e5381a3
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-15 18:41:52 +0900

    チャットボットのアイコン追加

--- src/ChatBox.jsx
+++ src/ChatBox.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import A from "../images/A.png";
+import B from "../images/B.png";
+import C from "../images/C.png"; 
 
 const ChatBox = ({ messages, error }) => {
+  const getImageSrc = (name) => {
+    switch (name) {
+      case 'A':
+        return A;
+      case 'B':
+        return B;
+      case 'C':
+        return C;
+      default:
+        return ''; // デフォルトの画像パス、必要ならば設定
+    }
+  };
+
   return (
     <>
     <div className="chat-box">
       {messages.slice(1).map((msg, index) => (
-        <p key={index} className={`${msg.role} ${msg.name}`}>
-          {msg.content}
-        </p>
+        <div className={msg.name}>
+          <img src={getImageSrc(msg.name)} alt={msg.name} />
+          <p key={index} className={msg.role}>
+            {msg.content}
+          </p>
+        </div>
       ))}
     </div>
     {error && <p style={{ color: 'red' }}>Error: {error.message}</p>}
