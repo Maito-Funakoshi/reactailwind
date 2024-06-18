af1837598de3eb55e694f736b20e49bcd754e29e
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-06-18 23:44:27 +0900

    同じく

--- src/App.jsx
+++ src/App.jsx
@@ -75,12 +75,13 @@ function App() {
   //変数設定
   const [inputAble, setInputAble] = useState(true);
   const [error, setError] = useState(null);
-  // const [recipients, setRecipients] = useState(
-  //   names.reduce((acc, name) => {
-  //     acc[name] = true;
-  //     return acc;
-  //   }, {})
-  // );
+  //###あとで消す###
+  const [recipients, setRecipients] = useState(
+    names.reduce((acc, name) => {
+      acc[name] = true;
+      return acc;
+    }, {})
+  );
 
   //HTML部分
   return (
