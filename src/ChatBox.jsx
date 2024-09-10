952970de8f5c7e793fdbe69abcb5d0176b386413
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-09-11 01:36:32 +0900

    自動スクロール問題一応解決

--- src/ChatBox.jsx
+++ src/ChatBox.jsx
@@ -40,8 +40,7 @@ const ChatBox = ({ names, namesEng, messages, error }) => {
     if (chatBoxRef.current) {
       var element = document.documentElement;
       var bottom = element.scrollHeight - element.clientHeight;
-      console.log(chatBoxRef.current.clientHeight);
-      if (chatBoxRef.current.clientHeight < 718) {
+      if (chatBoxRef.current.clientHeight < 718) {// body.height(=778px) - h1.height(60px)
         bottom = 0;
       }
       window.scrollTo({ top: bottom, behavior: "smooth" });
