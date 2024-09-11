fbe92de6caa51775ee51a9487f053a41431c99f4
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-09-11 10:15:01 +0900

    ログボタン関係コメントアウト

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import downloadIcon from "../images/downloadIcon.png"
+// import downloadIcon from "../images/downloadIcon.png"
 
 const Header = ({ messages, inputAble, setInputAble}) => {
   const reflect = () => {
     setInputAble(!inputAble);
   };
 
-  const handleGetLog = () => {
-    const log = messages.map((msg) =>msg.name + ": " + msg.content).join('\n');
-    downloadLogFile(log);
-  };
+  // const handleGetLog = () => {
+  //   const log = messages.map((msg) =>msg.name + ": " + msg.content).join('\n');
+  //   downloadLogFile(log);
+  // };
 
-  const downloadLogFile = (log) => {
-    const element = document.createElement("a");
-    const file = new Blob([log], { type: 'text/plain' });
-    element.href = URL.createObjectURL(file);
-    element.download = "conversation_log.txt";
-    document.body.appendChild(element);
-    element.click();
-  };
+  // const downloadLogFile = (log) => {
+  //   const element = document.createElement("a");
+  //   const file = new Blob([log], { type: 'text/plain' });
+  //   element.href = URL.createObjectURL(file);
+  //   element.download = "conversation_log.txt";
+  //   document.body.appendChild(element);
+  //   element.click();
+  // };
 
   return (
     <header className="App-header">
