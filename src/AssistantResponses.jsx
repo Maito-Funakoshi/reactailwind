0aafab56f8a65c8c7003930084e3971c6222b9cd
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-09-14 17:35:05 +0900

    maxContextMessages削除

--- src/AssistantResponses.jsx
+++ src/AssistantResponses.jsx
@@ -8,17 +8,14 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, inputAble,
     dangerouslyAllowBrowser: true
   })
 
-  const maxContextMessages = 100;
-
   useEffect(() => {
     if (inputAble) {
       if (messages.length > 4　&& messages[messages.length - 1].role == "user") {
         const makeResponse = async () => {
-          let currentMessages = [...messages].slice(-maxContextMessages);
               try {
                 const chatMessages = [
                   { role: "system", content: `あなたは${names[0]}という名前のアシスタントです。以下の設定をもとに返答を作成してください。${chat} あなたの特徴は以下の通りです。${characters[0]}` },
-                    ...currentMessages.map(message => ({...message, role: "user"}))
+                    ...messages.map(message => ({...message, role: "user"}))
                 ];
                 let response = await openai.chat.completions.create({
                   model: "gpt-4o",
@@ -58,7 +55,6 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, inputAble,
                 if (response.choices && response.choices.length > 0) {
                   const botMessage = response.choices[0].message.content.trim();
                   const assistantMessage = { role: "assistant", content: `${botMessage}`, name: `${namesEng[0]}`, mode: "chat"};
-                  currentMessages = [...currentMessages, assistantMessage];
                   setMessages(prevMessages => [...prevMessages, assistantMessage]);
                 }
               } catch (err) {
