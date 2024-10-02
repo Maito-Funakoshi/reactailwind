3fcf436e056b2a9a7ed33a4d3242fa0032e32ff6
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-10-03 01:47:10 +0900

    chatGPTの提案を一部導入

--- src/AssistantResponses.jsx
+++ src/AssistantResponses.jsx
@@ -8,148 +8,13 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, theme, set
     dangerouslyAllowBrowser: true
   })
 
-  // useEffect(() => {
-  //   if (inputAble) {
-  //     if (messages.length > 4　&& messages[messages.length - 1].role == "user") {
-  //       const makeResponse = async () => {
-  //             try {
-  //               const chatMessages = [
-  //                 { role: "system", content: `あなたは${names[0]}という名前のアシスタントです。以下の設定をもとに返答を作成してください。${chat} ユーザのお悩み「${theme}」から話題が離れないようにしてください。あなたの特徴は以下の通りです。${characters[0]}` },
-  //                   ...messages.map(message => ({...message, role: "user"}))
-  //               ];
-  //               let response = await openai.chat.completions.create({
-  //                 model: "gpt-4o",
-  //                 messages: chatMessages,
-  //                 temperature: 1.2
-  //               })
-  //               console.log(response.choices[0].message.content.trim())
-
-  //               // 発言様式を整備する
-  //               const commonMessages = [
-  //                   { role: "system", content: `${common}`},
-  //                   { role: "user", content: `${response.choices[0].message.content.trim()}`}
-  //               ];
-  //               response = await openai.chat.completions.create({
-  //                 model: "gpt-4o",
-  //                 messages: commonMessages,
-  //                 temperature: 1.2
-  //               })
-  //               console.log(response.choices[0].message.content.trim())
-
-  //               // // その他修正を適宜する
-  //               // const complementMessages = [
-  //               //     { role: "system", content: `${complementChat}`},
-  //               //     { role: "user", content: `${response.choices[0].message.content.trim()}`}
-  //               // ]
-  //               // response = await clients[0].getChatCompletions(deploymentId, complementMessages);
-
-  //               // // 返答を要約する
-  //               // const summaryMessages = [
-  //               //     { role: "system", content: `${summary}`},
-  //               //     { role: "user", content: `${response.choices[0].message.content.trim()}`}
-  //               // ]
-  //               // response = await openai.chat.completions.create({
-  //               //   model: "gpt-4o",
-  //               //   messages: summaryMessages,
-  //               //   temperature: 1.2
-  //               // })
-
-  //               if (response.choices && response.choices.length > 0) {
-  //                 const botMessage = response.choices[0].message.content.trim();
-  //                 const assistantMessage = { role: "assistant", content: `${botMessage}`, name: `${namesEng[0]}`, mode: "chat"};
-  //                 setMessages(prevMessages => [...prevMessages, assistantMessage]);
-  //               }
-  //             } catch (err) {
-  //               setError(err);
-  //               console.error("The sample encountered an error:", err);
-  //             }
-  //       }
-  //       if (messages.length == 5){
-  //         setTheme(messages[messages.length - 1]);
-  //       }
-  //       makeResponse();
-  //     }
-  //   }
-  //   else if (!inputAble) {
-  //       if (messages.length > 4 && messages[messages.length - 2].role == "user") {
-  //           const makeResponse = async () => {
-  //               for(let i = 0; i < names.length * reflectChatCount; i++){
-  //                   let j = i % names.length;
-  //                   try {
-  //                       const reflectMessages = [
-  //                           { role: "system", content: `あなたは${names[j]}という名前のアシスタントで、${names[(j + 1) % names.length]}と${names[(j + 2) % names.length]}に向かって話しています。以下の設定をもとに返答を作成してください。${reflect} ユーザのお悩み「${theme}」から話題が離れないようにしてください。あなたの特徴は以下の通りです。${characters[j]}` },
-  //                           ...messages.map(message => ({ ...message, role: "user" }))
-  //                       ];
-  //                       let response = await openai.chat.completions.create({
-  //                         model: "gpt-4o",
-  //                         messages: reflectMessages,
-  //                         // 4802=？　30=?　177776=あなた　157351=でしょう　44900=ください　42993=ょ　103554=しょう　7128=か　165732=かな　177401=ユー　25885=私　16407=思　15121=です　14429=ます
-  //                         logit_bias: {4802:-100, 30:-100, 177776:-100, 177401:2.5, 25885:2.5, 16407:2.5, 15121:2.5, 14429:2.5},
-  //                         temperature: 1.2
-  //                       })
-  //                       console.log(response.choices[0].message.content.trim())
-
-  //                       // 発言様式を整備する
-  //                       const commonMessages = [
-  //                           { role: "system", content: `${common}` },
-  //                           { role: "user", content: `${response.choices[0].message.content.trim()}` }
-  //                       ];
-  //                       response = await openai.chat.completions.create({
-  //                         model: "gpt-4o",
-  //                         messages: commonMessages,
-  //                         temperature: 1.2
-  //                       })
-  //                       console.log(response.choices[0].message.content.trim())
-
-  //                       // // その他修正を適宜する
-  //                       // const complementMessages = [
-  //                       //     { role: "system", content: `${complementReflect}` },
-  //                       //     { role: "user", content: `${response.choices[0].message.content.trim()}` }
-  //                       // ];
-  //                       // response = await clients[j].getChatCompletions(deploymentId, complementMessages);
-
-  //                       // // 返答を要約する
-  //                       // const summaryMessages = [
-  //                       //     { role: "system", content: `${summary}` },
-  //                       //     { role: "user", content: `${response.choices[0].message.content.trim()}` }
-  //                       // ];
-  //                       // response = await openai.chat.completions.create({
-  //                       //   model: "gpt-4o",
-  //                       //   messages: summaryMessages,
-  //                       //   logit_bias: {4802:-100, 30:-100, 177776:-100, 157351:-100, 44900:-100, 42993:-100, 103554:-100, 7128:-100, 165732:-100, 177401:2, 25885:2, 16407:2, 15121:2, 14429:2},
-  //                       //   temperature: 1.2
-  //                       // })
-
-  //                       if (response.choices && response.choices.length > 0) {
-  //                           const botMessage = response.choices[0].message.content.trim();
-  //                           const assistantMessage = { role: "assistant", content: `${botMessage}`, name: `${namesEng[j]}`, mode: "reflect" };
-  //                           setMessages(prevMessages => [...prevMessages, assistantMessage]); 
-  //                       }
-  //                       if (i == names.length * reflectChatCount - 1) {
-  //                           setMessages(prevMessages => [...prevMessages, {role: "assistant", content: `${endReflectingMessage}`, name: `${namesEng[0]}`, mode: "chat" }]); 
-  //                       }
-  //                   } catch (err) {
-  //                       setError(err);
-  //                       console.error("The sample encountered an error:", err);
-  //                   }
-  //               }
-  //           };
-  //           makeResponse();
-  //       } else {
-  //           setInputAble(!inputAble);
-  //       }
-  //   }
-  // }, [messages, inputAble]);
-
-
-
   useEffect(() => {
     if (inputAble) {
       if (messages.length > 4　&& messages[messages.length - 1].role == "user") {
         const makeResponse = async () => {
               try {
                 const chatMessages = [
-                  { role: "system", content: `あなたは心理カウンセラーです。ユーザーとの会話でオープンダイアローグの原則に従って、共感的かつ受容的な姿勢で対話を行ってください。質問を通じてユーザーが自身の感情や問題に気づけるように導いてください。ユーザーの発言に対して深い洞察を示し、次の質問や確認のための発言を行ってください。` },
+                  { role: "system", content: `あなたは${names[0]}という名前の心理カウンセラーです。ユーザーとの会話でオープンダイアローグの原則に従って、共感的かつ受容的な姿勢で対話を行ってください。ユーザのお悩み「${theme}」から話題が離れないようにしてください。質問を通じてユーザーが自身の感情や問題に気づけるように導いてください。ユーザーの発言に対して深い洞察を示し、次の質問や確認のための発言を行ってください。` },
                     ...messages.map(message => ({...message, role: "user"}))
                 ];
                 let response = await openai.chat.completions.create({
@@ -157,6 +22,36 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, theme, set
                   messages: chatMessages,
                   temperature: 1.2
                 })
+                console.log(response.choices[0].message.content.trim())
+
+                // // 発言様式を整備する
+                // const commonMessages = [
+                //     { role: "system", content: `${common}`},
+                //     { role: "user", content: `${response.choices[0].message.content.trim()}`}
+                // ];
+                // response = await openai.chat.completions.create({
+                //   model: "gpt-4o",
+                //   messages: commonMessages,
+                //   temperature: 1.2
+                // })
+
+                // // その他修正を適宜する
+                // const complementMessages = [
+                //     { role: "system", content: `${complementChat}`},
+                //     { role: "user", content: `${response.choices[0].message.content.trim()}`}
+                // ]
+                // response = await clients[0].getChatCompletions(deploymentId, complementMessages);
+
+                // // 返答を要約する
+                // const summaryMessages = [
+                //     { role: "system", content: `${summary}`},
+                //     { role: "user", content: `${response.choices[0].message.content.trim()}`}
+                // ]
+                // response = await openai.chat.completions.create({
+                //   model: "gpt-4o",
+                //   messages: summaryMessages,
+                //   temperature: 1.2
+                // })
 
                 if (response.choices && response.choices.length > 0) {
                   const botMessage = response.choices[0].message.content.trim();
@@ -168,6 +63,9 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, theme, set
                 console.error("The sample encountered an error:", err);
               }
         }
+        if (messages.length == 5){
+          setTheme(messages[messages.length - 1]);
+        }
         makeResponse();
       }
     }
@@ -178,15 +76,49 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, theme, set
                     let j = i % names.length;
                     try {
                         const reflectMessages = [
-                            { role: "system", content: `あなたは心理カウンセリングにおいて、リフレクティング（反射的傾聴）を行っています。ユーザーがこれまで話してきた内容を振り返り、3人の治療者として、それぞれ異なる観点から感じたことや考えたことを述べてください。リフレクティングは、ユーザーの発言を整理し、理解を深め、内省を促すことを目的としています。3人の治療者が順番にリフレクティングを行い、最終的にユーザーが話したことに対して丁寧にまとめ、今後の展開に繋がる発言をしてください。` },
+                            { role: "system", content: `あなたは「${characters[j]}」という特徴を持つ${names[j]}という名前の心理カウンセラーでリフレクティング（反射的傾聴）を行っており、${names[(j + 1) % names.length]}と${names[(j + 2) % names.length]}に向かって話しています。リフレクティングは、ユーザーの発言を整理し、理解を深め、内省を促すことを目的としています。ユーザのお悩み「${theme}」から話題が離れないようにしてください。ユーザーがこれまで話してきた内容を振り返り、感じたことや考えたことを述べてください。今後の展開に繋がる発言をしてください。` },
                             ...messages.map(message => ({ ...message, role: "user" }))
                         ];
                         let response = await openai.chat.completions.create({
                           model: "gpt-4o",
                           messages: reflectMessages,
+                          // 4802=？　30=?　177776=あなた　157351=でしょう　44900=ください　42993=ょ　103554=しょう　7128=か　165732=かな　177401=ユー　25885=私　16407=思　15121=です　14429=ます
+                          // logit_bias: {4802:-100, 30:-100, 177776:-100, 177401:2.5, 25885:2.5, 16407:2.5, 15121:2.5, 14429:2.5},
                           temperature: 1.2
                         })
-   
+                        console.log(response.choices[0].message.content.trim())
+
+                        // 発言様式を整備する
+                        const commonMessages = [
+                            { role: "system", content: `${common}` },
+                            { role: "user", content: `${response.choices[0].message.content.trim()}` }
+                        ];
+                        response = await openai.chat.completions.create({
+                          model: "gpt-4o",
+                          messages: commonMessages,
+                          temperature: 1.2
+                        })
+                        console.log(response.choices[0].message.content.trim())
+
+                        // // その他修正を適宜する
+                        // const complementMessages = [
+                        //     { role: "system", content: `${complementReflect}` },
+                        //     { role: "user", content: `${response.choices[0].message.content.trim()}` }
+                        // ];
+                        // response = await clients[j].getChatCompletions(deploymentId, complementMessages);
+
+                        // // 返答を要約する
+                        // const summaryMessages = [
+                        //     { role: "system", content: `${summary}` },
+                        //     { role: "user", content: `${response.choices[0].message.content.trim()}` }
+                        // ];
+                        // response = await openai.chat.completions.create({
+                        //   model: "gpt-4o",
+                        //   messages: summaryMessages,
+                        //   logit_bias: {4802:-100, 30:-100, 177776:-100, 157351:-100, 44900:-100, 42993:-100, 103554:-100, 7128:-100, 165732:-100, 177401:2, 25885:2, 16407:2, 15121:2, 14429:2},
+                        //   temperature: 1.2
+                        // })
+
                         if (response.choices && response.choices.length > 0) {
                             const botMessage = response.choices[0].message.content.trim();
                             const assistantMessage = { role: "assistant", content: `${botMessage}`, name: `${namesEng[j]}`, mode: "reflect" };
